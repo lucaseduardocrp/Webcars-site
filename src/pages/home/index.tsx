45a2df7b8c6fda09f9e6698157338cac7749dff2
lucaseduardocrp <lucaseduardocrp@gmail.com>
2023-09-07 12:32:34 -0300

    .

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,6 +9,7 @@ import { TCars } from '../../types/TCars';
 
 export const Home = () => {
   const [cars, setCars] = useState<TCars[]>([]);
+  const [loadImages, setLoadImages] = useState<string[]>([]);
 
   useEffect(() => {
     function loadCars() {
@@ -38,6 +39,10 @@ export const Home = () => {
     loadCars();
   }, []);
 
+  function handleImageLoad(id: string) {
+    setLoadImages((prevImageLoaded) => [...prevImageLoaded, id]);
+  }
+
   return (
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -51,7 +56,17 @@ export const Home = () => {
         {cars.map((car) => (
           <Link key={car.id} to={`/car/${car.id}`}>
             <section className="w-full bg-white rounded-lg">
-              <img src={car.images[0].url} alt="Carro" className="w-full rounded-lg mb-2 max-h-72" />
+              <div
+                className="w-full h-72 rounded-lg bg-slate-200"
+                style={{ display: loadImages.includes(car.id) ? 'none' : 'block' }}
+              ></div>
+              <img
+                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                src={car.images[0].url}
+                alt="Carro"
+                onLoad={() => handleImageLoad(car.id)}
+                style={{ display: loadImages.includes(car.id) ? 'block' : 'none' }}
+              />
               <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
 
               <div className="flex flex-col px-2">
